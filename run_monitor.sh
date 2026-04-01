c213dd2a2df4a248e9a23a441ba0baa273455d2f
Zhengran Ji <zj90@bc298-cmp-06.egr.duke.edu>
2026-04-01 17:49:05 -0400

    save work

--- run_monitor.sh
+++ run_monitor.sh
@@ -2,9 +2,12 @@
 
 THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 
+echo $THIS_FILE_DIR
+
 . $THIS_FILE_DIR/kill_monitor.sh
 
 # select input device 10 to prevent conflicting with ssh
 while read -u10 NODE_NAME; do
 	ssh $NODE_NAME bash "$THIS_FILE_DIR/run_single_node.sh" $NODE_NAME "$THIS_FILE_DIR/nodestats/"
+	echo "Started monitoring on $NODE_NAME"
 done 10< "$THIS_FILE_DIR/node-list.txt"
--- run_single_node.sh
+++ run_single_node.sh
@@ -6,5 +6,7 @@ OUT_DIR=$2
 
 THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 
-nohup python "$THIS_FILE_DIR/$SCRIPT_NAME" $MACHINE_NAME $OUT_DIR > monitor-$MACHINE_NAME.log 2>&1 &
+echo $THIS_FILE_DIR
+
+nohup python "$THIS_FILE_DIR/$SCRIPT_NAME" $MACHINE_NAME $OUT_DIR > "$THIS_FILE_DIR/monitor-$MACHINE_NAME.log" 2>&1 &
 echo "Started script on $MACHINE_NAME"
