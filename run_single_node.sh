fd39fb09eceed5554f807fd6aec3888946be5670
Zhengran Ji <zj90@bc298-cmp-06.egr.duke.edu>
2026-03-31 23:04:27 -0400

    s

--- run_single_node.sh
+++ run_single_node.sh
@@ -6,5 +6,5 @@ OUT_DIR=$2
 
 THIS_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 
-nohup python "$THIS_FILE_DIR/$SCRIPT_NAME" $MACHINE_NAME $OUT_DIR > /tmp/monitor-$MACHINE_NAME.log 2>&1 &
+nohup python "$THIS_FILE_DIR/$SCRIPT_NAME" $MACHINE_NAME $OUT_DIR > monitor-$MACHINE_NAME.log 2>&1 &
 echo "Started script on $MACHINE_NAME"
